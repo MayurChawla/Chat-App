79b3bbedde74badb10b59e7a0fd43822ae042a91
MayurChawla <mr.mayurchawla@gmail.com>
2021-05-06 18:47:15 +0530

    removed spaces

--- functions/index.js
+++ functions/index.js
@@ -13,25 +13,19 @@ exports.detectEvilUsers = functions.firestore
  const filter = new Filter();
  const { text, uid } = doc.data(); 
 
-
- if (filter.isProfane(text)) {
-
-     const cleaned = filter.clean(text);
-     await doc.ref.update({text: `🤐 I got BANNED for life for saying... ${cleaned}`});
-
-     await db.collection('banned').doc(uid).set({});
- } 
-
- const userRef = db.collection('users').doc(uid)
-
- const userData = (await userRef.get()).data();
-
- if (userData.msgCount >= 7) {
-     await db.collection('banned').doc(uid).set({});
- } else {
-     await userRef.set({ msgCount: (userData.msgCount || 0) + 1 })
- }
-                    });
+    if (filter.isProfane(text)) {
+        const cleaned = filter.clean(text);
+        await doc.ref.update({text: `🤐 I got BANNED for life for saying... ${cleaned}`});
+        await db.collection('banned').doc(uid).set({});
+    } 
+    const userRef = db.collection('users').doc(uid)
+    const userData = (await userRef.get()).data();
+    if (userData.msgCount >= 7) {
+        await db.collection('banned').doc(uid).set({});
+    } else {
+        await userRef.set({ msgCount: (userData.msgCount || 0) + 1 })
+    }
+    });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
