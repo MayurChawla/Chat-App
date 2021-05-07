d2c1f1efd36c6a805caedab8085480b131931120
MayurChawla <mr.mayurchawla@gmail.com>
2021-05-07 11:20:18 +0530

    removed key

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBiYGfg9oDagFnSri8RmGJRQ9WHhGoQ9lw",
-    authDomain: "firechat-d386e.firebaseapp.com",
-    projectId: "firechat-d386e",
-    storageBucket: "firechat-d386e.appspot.com",
-    messagingSenderId: "981451750877",
-    appId: "1:981451750877:web:9eb3df9f21eddf13a80a34"
+    
 });
 
 const auth = firebase.auth();
